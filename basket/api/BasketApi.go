5c5f8b2eef2049b159576eddced39e3a611dae54
kerami <keramiozsoy@hotmail.com>
2019-04-20 00:35:55 +0300

    some new method

--- basket/api/BasketApi.go
+++ basket/api/BasketApi.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"log"
 	"net/http"
 )
 
-func productApiHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+type myData struct {
+	Name    string
+	Surname string
 }
 
-func productAPIMethod() string {
-	http.HandleFunc("/productapi", productApiHandler)
-	return "Hello, '/productapi'"
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path+"-"+r.Method))
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+
+	var data myData
+	err := decoder.Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+
+	name := data.Name
+	surname := data.Surname
+
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path+" -> "+r.Method+" -> "+name+" -> "+surname))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func apiMethods() string {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/basket/list", listHandler)
+	http.HandleFunc("/basket/save", saveHandler)
+	return "Hello, '/basket'"
 }
 
 func main() {
-	productAPIMethod()
+	apiMethods()
 	log.Fatal(http.ListenAndServe(":8002", nil))
 }
